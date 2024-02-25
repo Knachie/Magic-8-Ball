b3a39e60777d5a9d27762286dc47fc2e2baeec92
Kachi Ugwoke <kchugw@gmail.com>
2024-02-25 16:17:37 +0100

    Add project files.

---
+++ Magic 8 Ball/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Magic_8_Ball
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
