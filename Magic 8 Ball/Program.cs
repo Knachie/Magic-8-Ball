adf87e4603aa4ddd511ada0a66022de0c1be8431
Kachi Ugwoke <kchugw@gmail.com>
2024-02-26 14:43:10 +0100

    added the function to answer a question

--- Magic 8 Ball/Program.cs
+++ Magic 8 Ball/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine(Answer());
+            }
+        }
+
+        static string Answer()
+        {
+            string result = string.Empty;
+            Random random = new Random();
+
+            string[] answers = { "it is a possibility", "it is certain", "you may rely on it", "the chanses are slim", "don't count on it", "my reply is no", "all signs point to yes"};
+            result = answers[random.Next(answers.Length)];
+
+            return result;
         }
     }
 }
