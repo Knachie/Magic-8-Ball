e2df5bf22b026c63f532e4253d04b13910734652
Kachi Ugwoke <kchugw@gmail.com>
2024-02-26 15:02:04 +0100

    Entered the questioning mechanic

--- Magic 8 Ball/Program.cs
+++ Magic 8 Ball/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Question();
+        }
+
+        static void Question()
+        {
+            Console.WriteLine("Enter your question?");
+            string question = Console.ReadLine();
+
+            Console.Clear();
+            Console.Write("Fetching oracle");
+            for(int i = 0; i < 3; i++)
+            {
+                Task.Delay(1000).Wait();
+                Console.Write('.');
+            }
+            Task.Delay(1000).Wait();
+            Console.Clear();
+            Console.WriteLine("Question: " + question + "\n\nAnswer: " + "hello");
         }
     }
 }
